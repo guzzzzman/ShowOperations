86c21066ea6d667045856ddcc34214f8c0a1ca2b
guzzzzman <155527944+guzzzzman@users.noreply.github.com>
2024-01-03 22:11:40 -0800

    Add files via upload

---
+++ ShowOperations.java
@@ -0,0 +1,36 @@
+// Name: Jose Guzman
+// Date: 9/3/2023
+// File Name: ShowOperations.java
+// Description: To print the calculations of expressions
+
+public class ShowOperations
+{
+  public static void main(String[] args)
+  {
+    System.out.println( "15.0 + 2.0 = " + (15.0+2.0)+ "\n"
+                      + "15.0 - 2.0 = " + (15.0-2.0) + "\n"
+                      + "15.0 * 2.0 = " + (15.0*2.0) + "\n"
+                      + "15.0 / 2.0 = " + (15.0/2.0) + "\n"
+                      + "3.0 * 5.0 = " + (3.0*5.0) + "\n"
+                      + "7.1 * 8.3 - 2.2 " + (7.1*8.3-2.2) + "\n"
+                      + "3.2 / (6.1 * 5) = " + (3.2 / (6.1 * 5)));
+       // text in quotes ^^^^^^^^^^^^^ 
+       // equations to be calculated      ^^^^^^^^^^
+       // "\n" - inserts a new line
+
+    System.out.println("15 / 2 = " + (15/2) );
+            
+  }
+}
+
+/*
+
+15.0 + 2.0 = 17.0
+15.0 - 2.0 = 13.0
+15.0 * 2.0 = 30.0
+15.0 / 2.0 = 7.5
+15 / 2 = 7
+3.0 * 5.0 = 15.0
+7.1 * 8.3 - 2.2 = 56.73
+3.2 / (6.1 * 5) = 0.10491803278688525
+*/
